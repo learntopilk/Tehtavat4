328e76edf2ecda94172eea7c0cf2ed770cec82dc
learntopilk <jonsterivilja@gmail.com>
2018-05-31 20:06:25 +0300

    Tehtävä 4.4 tehty
    Ja testit menevät läpi.

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,11 +2,21 @@
 
 const dummy = (blogs) => {
   // ...
-  console.log("Here be blogs (or frogs?).", blogs)
+  console.log('Here be blogs (or frogs?).', blogs)
   return 1
+}
+
+const totalLikes = (blogs) => {
+
+  let i = 0
+  blogs.forEach(b => {i += b.likes})
+
+  console.log(i)
+  return i
+
 }
 
 
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
