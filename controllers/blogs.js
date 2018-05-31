eda94d5a450220b336eded273cb3205b9d0252a6
learntopilk <jonsterivilja@gmail.com>
2018-05-31 18:47:28 +0300

    Tehtävä 4.2 tehty
    Koodi jaettu moduuleihin

---
+++ controllers/blogs.js
@@ -0,0 +1,50 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+const formatBlogPost = (req) => {
+    return {
+        title: req.body.title,
+        author: req.body.author,
+        url: req.body.url,
+        likes: req.body.likes
+    }
+}
+
+
+
+
+blogsRouter.get('/', (request, response) => {
+    Blog
+        .find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
+        .catch(err => {
+            console.log("error getting blog posts: ", err)
+            response.status(400).json({ error: "Something went wrong" })
+        })
+})
+
+blogsRouter.post('/', (request, response) => {
+    console.log("request.body: ", request.body)
+    if (request.body.title && request.body.author && request.body.url && request.body.likes) {
+
+        const blog = new Blog(formatBlogPost(request))
+
+        blog
+            .save()
+            .then(result => {
+                console.log("Post saved")
+                response.status(201).json(result)
+            })
+            .catch(err => {
+                console.log(err)
+                response.status(500).json({ error: "something went wrong" })
+            })
+    } else {
+        response.status(403).json({error: "Bad request"})
+    }
+
+})
+
+module.exports = blogsRouter
--- index.js
+++ index.js
@@ -4,68 +4,22 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs.js')
 require('dotenv').config()
 
-const Blog = mongoose.model('Blog', {
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-
-module.exports = Blog
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use('/api/blogs', blogsRouter)
 
 const mongoUrl = process.env.DBURL
 mongoose.connect(mongoUrl)
+    .then(console.log("DB up and connected"))
+    .catch(err => console.log(err))
 console.log(mongoUrl)
 
-const formatBlogPost = (req) => {
-    return {
-        title: req.body.title,
-        author: req.body.author,
-        url: req.body.url,
-        likes: req.body.likes
-    }
-}
+const PORT = process.env.PORT || 3003
 
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-        .catch(err => {
-            console.log("error getting blog posts: ", err)
-            response.status(400).json({ error: "Something went wrong" })
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    console.log("request.body: ", request.body)
-    if (request.body.title && request.body.author && request.body.url && request.body.likes) {
-
-        const blog = new Blog(formatBlogPost(request))
-
-        blog
-            .save()
-            .then(result => {
-                console.log("Post saved")
-                response.status(201).json(result)
-            })
-            .catch(err => {
-                console.log(err)
-                response.status(500).json({ error: "something went wrong" })
-            })
-    } else {
-        response.status(403).json({error: "Bad request"})
-    }
-
-})
-
-const PORT = 3003
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
