68eb78866869e3566a64b27531043b8a817d17f4
learntopilk <jonsterivilja@gmail.com>
2018-05-31 20:14:17 +0300

    Tehtävä 4.5 tehty
    Testit tehty ja läpäistään - suosituin blogi saadaan selville.

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -11,12 +11,26 @@ const totalLikes = (blogs) => {
   let i = 0
   blogs.forEach(b => {i += b.likes})
 
-  console.log(i)
   return i
 
 }
 
+const favoriteBlog = (blogs) => {
+  let mostLikedBlog = {}
+  let mostLikes = 0
+
+  blogs.forEach(blog => {
+    if (blog.likes > mostLikes) {
+      mostLikedBlog = blog
+      mostLikes = blog.likes
+    }
+  })
+
+  console.log(mostLikedBlog)
+  return mostLikedBlog
+}
+
 
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
